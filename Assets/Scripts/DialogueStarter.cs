b594fa234406076069955371b926d238dac4f24a
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2026-06-18 15:32:53 -0300

    grasping concepts

--- Assets/Scripts/DialogueStarter.cs
+++ Assets/Scripts/DialogueStarter.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 public class DialogueStarter : MonoBehaviour
 {
     public string Dialogue;
-    void Start()
-    {
-        DialogueManager.StartConversation(Dialogue);
-    }
+    public bool IsClickNPC;
+    public bool OnceTime;
+    public bool DoneOnce = false;
+    public GameObject Notification;
+    public DialogueEntry entry;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            entry.currentDialogueText = Dialogue;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("yeah it work");
+            if (OnceTime)
+            {
+                return;
+            }
+            if (Notification != null)
+            {
+                Notification.SetActive(true);
+            }
+            if (IsClickNPC)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DialogueManager.StartConversation(Dialogue);
+                }
+            }
+        }
+    }
 }
